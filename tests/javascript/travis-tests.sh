6936479d5e1d09084f6b7e78006c33ba587c92d3
Yves Hoppe <yves@compojoom.com>
2016-07-31 10:51:58 +0200

    Added own matrix tests for JavaScript

---
+++ tests/javascript/travis-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script for installing firefox and fluxbox and preparing JavaScript tests
+
+BASE="$1"
+
+# Xvfb
+"export DISPLAY=:99.0"
+"sh -e /etc/init.d/xvfb start"
+sleep 3 # give xvfb some time to start
+
+# Fluxbox
+sudo apt-get update -qq
+sudo apt-get install -y --force-yes firefox fluxbox
+fluxbox &
+sleep 3 # give fluxbox some time to start
+
+#Make sure all node modules are installed
+cd tests/javascript
+npm install
+cd $BASE
